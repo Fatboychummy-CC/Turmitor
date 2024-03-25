d2b029edb09b732ed94b46d2674c60cbeec5c2ac
Matthew W <fatboychummy@gmail.com>
2024-03-25 02:29:27 -0600

    Refactor startup.lua to separate turtle and control program execution

--- startup.lua
+++ startup.lua
@@ -2,11 +2,16 @@
 --- turtle redirect system. Mainly just a simple helper program to be put on a
 --- startup disk.
 
-if turtle then
-  -- Get the directory of this startup program.
-  local dir = fs.getDir(shell.getRunningProgram())
+-- Get the directory of this startup program.
+local dir = fs.getDir(shell.getRunningProgram())
+shell.setDir(dir)
 
-  -- CD into the directory of this program and run the turtle redirect system.
-  shell.setDir(dir)
+if turtle then
+  -- If we are on a turtle, run the turtle program.
   shell.run("turmitor_turtle.lua")
+else
+  -- If we are not on a turtle, run the control program.
+
+  sleep(5)
+  shell.run("turmitor_control.lua")
 end
