b532d9797c4126a9181a81926ca95e4d746f7b62
Matthew W <fatboychummy@gmail.com>
2024-03-25 21:11:58 -0600

    Convert to library format and add single modem network

--- lib/turmitor_control.lua
+++ lib/turmitor_control.lua
@@ -1,16 +1,20 @@
 --- Turmitor Control Helper Methods
 
+local expect = require "cc.expect".expect
 local logging = require "logging"
+local smn = require "single_modem_network"
 
 local context = logging.create_context("control")
 
+---@class TurmitorControl
+local TurmitorControl = {}
 ---@fixme We need to ensure we are only grabbing turtles from one connected modem, so we don't accidentally grab turtles from other networks.
 
 --- Start the turtles (reboots any currently running turtles, so as to make sure they are all on the same state).
-local function start_turtles()
+function TurmitorControl.start_turtles()
   context.info("Starting turtles.")
 
-  local turtles = table.pack(peripheral.find("turtle"))
+  local turtles = table.pack(smn.find("turtle"))
 
   for i = 1, turtles.n do
     context.info("Shutting down turtle ", i, ":", peripheral.getName(turtles[i]), ".")
@@ -26,50 +30,25 @@ local function start_turtles()
 end
 
 --- Stop the turtles (shuts down any currently running turtles).
-local function stop_turtles()
+function TurmitorControl.stop_turtles()
   context.info("Stopping turtles.")
 
-  local turtles = table.pack(peripheral.find("turtle"))
+  local turtles = table.pack(smn.find("turtle"))
 
   for i = 1, turtles.n do
     turtles[i].shutdown()
   end
 end
 
---- Find a position to put the specified item.
----@param item_name string The name of the item to find a space for.
----@param chest_list table The chest.list() table.
----@param chest_size integer The size of the chest.
----@return integer slot The slot to put the item in.
-local function get_space_for_item(item_name, chest_list, chest_size)
-  -- Prefer to put the item in a used slot. The item we want to move is in the last slot, so we ignore it (hence the -1)
-  for i = 1, chest_size - 1 do
-    if chest_list[i] and chest_list[i].name == item_name and chest_list[i].count < 64 then
-      return i
-    end
-  end
-
-  -- Otherwise find an empty slot.
-  for i = 1, chest_size do
-    if not chest_list[i] then
-      return i
-    end
-  end
-
-  error("No space for item " .. item_name .. " in chest.")
-end
-
 --- Grab the concrete back from every turtle, and insert it into the chest.
-local function steal_concrete()
-  local chest = peripheral.find("minecraft:chest") --[[@as Inventory]]
-  local chest_name = peripheral.getName(chest)
-  local size = chest.size()
+function TurmitorControl.steal_concrete()
+  local chest = smn.find("minecraft:chest") --[[@as Inventory]]
   if not chest then error("No chest found.") end
 
   context.info("Stealing concrete from turtles.")
 
   -- For each turtle...
-  for _, turtle in ipairs({peripheral.find("turtle")}) do
+  for _, turtle in ipairs({smn.find("turtle")}) do
     context.info("Stealing concrete from turtle ", peripheral.getName(turtle), ".")
 
     local parallel_pull_calls = {}
@@ -86,3 +65,12 @@ local function steal_concrete()
   end
 end
 
+--- Set the modem network to use.
+---@param modem_side computerSide The side of the modem to use.
+function TurmitorControl.set_modem(modem_side)
+  expect(1, modem_side, "string")
+
+  smn.set_modem(modem_side)
+end
+
+return TurmitorControl
