689e2c14b6f0993c3cf6372c0c9f7e0dd08e6c30
Matthew W <fatboychummy@gmail.com>
2024-03-25 22:08:05 -0600

    Add computercraft font and reader

---
+++ lib/read_fbmp.lua
@@ -0,0 +1,43 @@
+return function(filename)
+  local file, err = fs.open(filename, "r")
+  if not file then
+    error(err, 2)
+  end
+
+  local header = file.readLine()
+  if not header then
+    error("Invalid file", 2)
+  end
+
+  local header_name, width = header:match("^(....)(..)$")
+  if header_name ~= "FBMP" then
+    error("Invalid header: " .. header_name)
+  end
+  width = string.unpack("<I2", width)
+
+  local data = {}
+
+  for line in file.readLine do
+    -- expand bits to black or white
+    local row = {}
+    local current_pos = 1
+    for i = 1, #line do
+      local byte = string.unpack("<B", line, i)
+
+      for j = 0, 7 do
+        if current_pos + j > width then
+          break
+        end
+
+        row[#row + 1] = bit32.extract(byte, 7-j, 1) == 1 and '0' or 'f'
+      end
+      current_pos = current_pos + 8
+    end
+
+    table.insert(data, row)
+  end
+
+  file.close()
+
+  return data
+end
