998772824b16c884658b0cdc71eeb319379d57c2
Matthew W <fatboychummy@gmail.com>
2024-04-04 19:18:04 -0600

    Add startup.lua file for turtle client

---
+++ startup.lua
@@ -0,0 +1,22 @@
+if not turtle then return end -- Only run on turtles.
+
+-- Set the working directory, so require works properly.
+-- We use this in case we get set to `disk5` or something silly like that.
+shell.setDir(fs.getDir(shell.getRunningProgram()))
+
+-- Set the package path to include the lib directory.
+package.path = package.path .. ";lib/?.lua;lib/?/init.lua"
+
+-- Optional: Set logging level:
+-- local logging = require "logging"
+-- logging.set_level(logging.LOG_LEVEL.DEBUG)
+
+-- Import the turtle client.
+local turmitor_client = require "turmitor_client"
+
+-- Optional: Set whether or not the Turmitor clients are built horizontally
+-- or vertically. By default, this value is "vertical".
+-- turmitor_client.array_style = "horizontal"
+--- 
+-- Run the Turmitor client.
+turmitor_client.run()
