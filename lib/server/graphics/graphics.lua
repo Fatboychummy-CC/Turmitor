b9e13ea26217011fff6f4bbf94dfa92375d28a0a
Matthew W <fatboychummy@gmail.com>
2024-05-24 02:35:11 -0600

    Add initial graphics module

---
+++ lib/server/graphics/graphics.lua
@@ -0,0 +1,344 @@
+--- Graphical implementation of lines, circles, pixels, etc.
+
+---@class graphics_object
+---@field enabled boolean Whether the object should be drawn. Ignored by the buffer methods, so parent graphics implementations should handle this.
+---@field draw_order integer The order in which the object should be drawn. Ignored by the buffer methods, so parent graphics implementations should handle this.
+---@field x integer The x-coordinate of the object.
+---@field y integer The y-coordinate of the object.
+---@field color color The color of the object.
+---@field type string The type of the object.
+
+---@class graphics_object-pixel: graphics_object
+---@field type "pixel"
+
+---@class graphics_object-line: graphics_object
+---@field type "line"
+---@field x2 integer The x-coordinate of the end of the line.
+---@field y2 integer The y-coordinate of the end of the line.
+---@field thickness integer The thickness of the line.
+
+---@class graphics_object-ellipse: graphics_object
+---@field type "ellipse"
+---@field filled boolean Whether the circle should be filled.
+---@field x integer The x-coordinate of the center of the circle.
+---@field y integer The y-coordinate of the center of the circle.
+---@field thickness integer The thickness of the circle (fills both inwards and outwards).
+---@field a number The horizontal scaling factor.
+---@field b number The vertical scaling factor.
+
+---@class graphics_object-rectangle: graphics_object
+---@field type "rectangle"
+---@field width integer The width of the rectangle.
+---@field height integer The height of the rectangle.
+---@field filled boolean Whether the rectangle should be filled.
+---@field thickness integer The thickness of lines of the rectangle (acts as an outline).
+
+---@class graphics_object-text: graphics_object
+---@field type "text"
+---@field text string The text to draw.
+
+---@class graphics_object-image: graphics_object
+---@field type "image"
+---@field image image The image to draw.
+---@field animate boolean Whether the image should be animated (if it is an animated image).
+---@field frame integer The current frame of the image.
+
+---@class graphics
+local graphics = {}
+
+--- Initialize a buffer with the given size and color.
+---@param x_size integer The width of the buffer.
+---@param y_size integer The height of the buffer.
+---@param color color? The color to fill the buffer with.
+---@return color[][] buffer The buffer.
+function graphics.init_buffer(x_size, y_size, color)
+  local buffer = {}
+  color = color or colors.black
+
+  for y = 1, y_size do
+    buffer[y] = {}
+
+    for x = 1, x_size do
+      buffer[y][x] = color
+    end
+  end
+
+  return buffer
+end
+
+--- Debug a buffer to the console.
+---@param buffer color[][] The buffer to debug.
+function graphics.debug_buffer(buffer)
+  local file = fs.open("buffer.txt", "a")
+  file.writeLine("====")
+  for y = 1, #buffer do
+    local line = ""
+
+    for x = 1, #buffer[y] do
+      line = line .. colors.toBlit(buffer[y][x])
+    end
+
+    file.writeLine(line)
+  end
+
+  file.close()
+end
+
+local function set_buffer(buffer, x, y, color)
+  if not buffer[y] or not buffer[y][x] then
+    return
+  end
+
+  buffer[y][x] = color
+end
+
+--- Write a pixel to the given buffer.
+---@param object graphics_object-pixel The object to draw.
+---@param buffer color[][] The buffer to draw to.
+function graphics.pixel(object, buffer)
+  set_buffer(buffer, object.x, object.y, object.color)
+end
+
+--- Write a line to the given buffer.
+---@param object graphics_object-line The object to draw.
+---@param buffer color[][] The buffer to draw to.
+function graphics.line(object, buffer, print_debug_info)
+  if print_debug_info then
+    print("====")
+  end
+  local x1, x2, y1, y2 = object.x, object.x2, object.y, object.y2
+
+  -- Calculate the slope of the line.
+  local dx = x2 - x1
+  local dy = y2 - y1
+  local m = dy / dx
+
+  -- If the slope is less than 1, we can use the x-coordinate as the iterator.
+  --[[
+    ##
+      ##
+        ##
+          ##
+    and so on.
+  ]]
+  if print_debug_info then
+    print("m:", m)
+  end
+
+  if math.abs(m) < 1 then
+    -- Sort the x-coordinates so that x1 is always less than x2.
+    if object.x > object.x2 then
+      x1, x2 = object.x2, object.x
+      y1, y2 = object.y2, object.y
+    end
+
+    for y_offset = -math.floor(object.thickness / 2), math.ceil(object.thickness / 2) - 1 do
+      for x = x1, x2 do
+        local y = math.floor(m * (x - x1) + y1) + y_offset
+
+        set_buffer(buffer, x, y, object.color)
+      end
+    end
+  else
+    -- Otherwise, we use the y-coordinate as the iterator.
+    --[[
+      #
+      #
+       #
+       #
+      and so on.
+    ]]
+
+    -- Sort the y-coordinates so that y1 is always less than y2.
+    if object.y > object.y2 then
+      x1, x2 = object.x2, object.x
+      y1, y2 = object.y2, object.y
+    end
+
+    for x_offset = -math.floor(object.thickness / 2), math.ceil(object.thickness / 2) - 1 do
+      if print_debug_info then
+        print("x_offset:", x_offset)
+      end
+      for y = y1, y2 do
+        local x = math.floor((y - y1) / m + x1) + x_offset
+        if print_debug_info then
+          print("x,y:", x, y)
+        end
+
+        set_buffer(buffer, x, y, object.color)
+      end
+    end
+  end
+end
+
+--- Write an ellipse to the given buffer.
+---@param object graphics_object-ellipse The object to draw.
+---@param buffer color[][] The buffer to draw to.
+function graphics.ellipse(object, buffer)
+  -- Midpoint ellipse drawing algorithm. Stolen and adapted to lua shamelessly
+  -- from https://www.geeksforgeeks.org/mid-point-ellipse-drawing-algorithm/
+
+  local x_center, y_center, a, b = object.x, object.y, object.a, object.b
+
+  if a == 0 then return end
+
+  if b == 0 then
+    b = a
+  end
+
+  local a_sqr, b_sqr = a * a, b * b
+  local x, y = 0, b
+  local color = object.color
+
+  -- Initial decision parameter of region 1.
+  local d1 = b_sqr - a_sqr * b + 0.25 * a_sqr
+  local dx, dy = 2 * b_sqr * x, 2 * a_sqr * y
+
+  -- We need to save the values we plot (in case we want to fill the circle),
+  -- so we'll use a table to store the left and right-most points, so we can
+  -- fill in using scanlines.
+  ---@type color[][] [y][1] = leftmost x, [y][2] = rightmost x
+  local scanlines = {}
+
+  --- Plot a point on the circle, and update the scanlines table.
+  ---@param _x integer The x-coordinate of the point.
+  ---@param _y integer The y-coordinate of the point.
+  local function plot(_x, _y)
+    -- For thickness...
+    if object.thickness == 1 then
+      set_buffer(buffer, _x, _y, color)
+    else
+      for offset = -math.floor(object.thickness / 2), math.ceil(object.thickness / 2) - 1 do
+        set_buffer(buffer, _x, _y + offset, color)
+        set_buffer(buffer, _x + offset, _y, color)
+      end
+    end
+
+    if not scanlines[_y] then
+      scanlines[_y] = {}
+    end
+
+    if not scanlines[_y][1] or _x < scanlines[_y][1] then
+      scanlines[_y][1] = _x
+    end
+
+    if not scanlines[_y][2] or _x > scanlines[_y][2] then
+      scanlines[_y][2] = _x
+    end
+  end
+
+  -- For region 1.
+  while dx < dy do
+    -- Write the points based on 4-way symmetry.
+    plot(x_center + x, y_center + y)
+    plot(x_center - x, y_center + y)
+    plot(x_center + x, y_center - y)
+    plot(x_center - x, y_center - y)
+
+    -- Checking and updating value of decision parameter based on algorithm.
+    if d1 < 0 then
+      x = x + 1
+      dx = dx + 2 * b_sqr
+      d1 = d1 + dx + b_sqr
+    else
+      x = x + 1
+      y = y - 1
+      dx = dx + 2 * b_sqr
+      dy = dy - 2 * a_sqr
+      d1 = d1 + dx - dy + b_sqr
+    end
+  end
+
+  -- Decision parameter of region 2.
+  local d2 = b_sqr * (x + 0.5) * (x + 0.5)
+    + a_sqr * (y - 1) * (y - 1) - a_sqr * b_sqr
+
+  -- Plotting points of region 2.
+  while y >= 0 do
+    -- Write the points based on 4-way symmetry.
+    plot(x_center + x, y_center + y)
+    plot(x_center - x, y_center + y)
+    plot(x_center + x, y_center - y)
+    plot(x_center - x, y_center - y)
+
+    -- Checking and updating parameter value based on algorithm.
+    if d2 > 0 then
+      y = y - 1
+      dy = dy - 2 * a_sqr
+      d2 = d2 + a_sqr - dy
+    else
+      y = y - 1
+      x = x + 1
+      dx = dx + 2 * b_sqr
+      dy = dy - 2 * a_sqr
+      d2 = d2 + a_sqr + dx - dy
+    end
+  end
+
+  -- Finally, fill the ellipse if needed.
+  if object.filled then
+    for y, scanline in pairs(scanlines) do
+      for x = scanline[1], scanline[2] do
+        set_buffer(buffer, x, y, color)
+      end
+    end
+  end
+end
+
+--- Write a rectangle to the given buffer.
+---@param object graphics_object-rectangle The object to draw.
+---@param buffer color[][] The buffer to draw to.
+function graphics.rectangle(object, buffer)
+  -- If thickness > 1, then we can offset are start positions by -thickness, and
+  -- increase the width and height by 2 * thickness. 
+  local x, y, width, height = object.x, object.y, object.width, object.height
+  local n1 = object.thickness - 1
+  x = x - n1
+  y = y - n1
+  width = width + 2 * n1
+  height = height + 2 * n1
+
+  -- If the rectangle is filled, we can just make a buffer the size of the
+  -- rectangle and fill it with the color.
+  if object.filled then
+    for y_offset = 0, height - 1 do
+      for x_offset = 0, width - 1 do
+        set_buffer(buffer, x + x_offset, y + y_offset, object.color)
+      end
+    end
+
+    return
+  end
+
+  -- Copy the top and bottom lines.
+  for y_offset = 0, object.thickness - 1 do
+    for x_offset = 0, width - 1 do
+      set_buffer(buffer, x + x_offset, y + y_offset, object.color)
+      set_buffer(buffer, x + x_offset, y + height - y_offset - 1, object.color)
+    end
+  end
+
+  -- Copy the left and right lines.
+  for x_offset = 0, object.thickness - 1 do
+    for y_offset = 0, height - 1 do
+      set_buffer(buffer, x + x_offset, y + y_offset, object.color)
+      set_buffer(buffer, x + width - x_offset - 1, y + y_offset, object.color)
+    end
+  end
+end
+
+--- Write text to the given buffer.
+---@param object graphics_object-text The object to draw.
+---@param buffer color[][] The buffer to draw to.
+function graphics.text(object, buffer)
+  error("Not yet implemented.")
+end
+
+--- Write an image to the given buffer.
+---@param object graphics_object-image The object to draw.
+---@param buffer color[][] The buffer to draw to.
+function graphics.image(object, buffer)
+  error("Not yet implemented.")
+end
+
+return graphics
---
+++ lib/server/graphics/init.lua
@@ -0,0 +1,369 @@
+--- Graphics library for the turmitor server.
+
+local TurmitorServer = require "turmitor_server"
+local graphics = require "server.graphics.graphics"
+
+local inverted_colors = {}
+for k, v in pairs(colors) do
+  if type(v) == "number" then
+    inverted_colors[v] = k
+  end
+end
+
+--- Convert a color from blit to a color that can be used by the turmitor server.
+---@param hex string The blit color to convert.
+---@return color? valid_color The converted color, or nil if the color is invalid.
+local function from_blit(hex)
+  if #hex ~= 1 then return end
+  local n = tonumber(hex, 16)
+  if not n then return end
+
+  return inverted_colors[2 ^ n]
+end
+
+---@class line_data
+---@field text string[] The characters of the line.
+---@field fg string[] The foreground colors of the line, in blit format. A space means the color is transparent.
+---@field bg string[] The background colors of the line, in blit format. A space means the color is transparent.
+
+---@class frame
+---@field duration number? The number of seconds this frame should be displayed.
+---@field palette table<color, integer> The palette of the frame, mapping color indices to hexadecimal colors. Overrides the global palette.
+---@field data line_data[] The data of the frame.
+
+---@class image
+---@field type string The original type of the image file, like "bimg".
+---@field version string The version of the image file, in semver format.
+---@field animated boolean Whether the image file is animated.
+---@field secondsPerFrame number? The number of seconds each frame should be displayed, required if `animated` is true.
+---@field author string? The author of the image file.
+---@field description string? A description of the image file, set by the author.
+---@field creator string? The program or software that created the image file.
+---@field date string? The date the image file was created, ISO-8601 format.
+---@field width integer The width of the image, in characters.
+---@field height integer The height of the image, in characters.
+---@field palette table<color, integer>? The palette of the image, mapping color indices to hexadecimal colors.
+---@field frames frame[] The frames of the image.
+
+---@class turmitor_graphics_object : graphics_object
+---@field delete fun() Deletes the object from the pre-buffer.
+
+---@class TurmitorGraphics
+local turmitor_graphics = {}
+
+--- The pre-buffer stores active objects, like sprites, that are not yet
+--- actually flushed to buffer 1. This allows for simple movement of sprites
+--- without having to redraw the entire screen.
+---
+--- The pre-buffer is a list of tables, where each table is a sprite, or pixel
+--- information.
+---@type turmitor_graphics_object[]
+local pre_buffer = {}
+
+--- Buffer layer 1: All current changes are made to this buffer, but nothing
+--- here is drawn to the screen.
+---@type color[][]
+local buffer_1 = {}
+
+--- Buffer layer 2: When `.flush()` is called, buffer layer 1 is compared with
+--- this buffer, and only the differences are drawn to the screen. This buffer
+--- is then updated with the new contents.
+---@type color[][]
+local buffer_2 = {}
+
+--- If this is true, the screen will be updated after every change.
+local auto_update = false
+
+--- The size of the screen.
+local size = { x = 0, y = 0 }
+local _
+_, _, size.x, size.y = TurmitorServer.get_size()
+
+--- Sort everything in the pre-buffer by their draw order.
+local function sort_pre_buffer()
+  table.sort(pre_buffer, function(a, b)
+    return a.draw_order < b.draw_order
+  end)
+end
+
+--- Draw a graphics object to the first buffer.
+---@param object graphics_object The object to draw.
+local function draw(object)
+  print("Draw...")
+  if object.enabled and graphics[object.type] then
+    print("Enabled, calling graphics function")
+    graphics[object.type](object, buffer_1)
+  end
+end
+
+--- Merge the pre-buffer down from many sprite objects and pixel datum to a
+--- single buffer.
+local function merge_down()
+  sort_pre_buffer()
+
+  for _, object in ipairs(pre_buffer) do
+    print("Doodly doodling", object.type)
+    draw(object)
+  end
+end
+
+--- Flush the differences of the first buffer to the screen, and update the
+--- second buffer.
+local function flush()
+  ---@type BatchPixels
+  local changes = {}
+
+  for y = 1, size.y do
+    for x = 1, size.x do
+      if buffer_1[y][x] ~= buffer_2[y][x] then
+        changes[#changes + 1] = { x = x, y = y, color = buffer_1[y][x] }
+        buffer_2[y][x] = buffer_1[y][x]
+      end
+    end
+  end
+
+  TurmitorServer.set_pixels(changes)
+end
+
+--- Update the screen.
+local function update_screen()
+  graphics.debug_buffer(buffer_1)
+  graphics.debug_buffer(buffer_2)
+  merge_down()
+  graphics.debug_buffer(buffer_1)
+  graphics.debug_buffer(buffer_2)
+  flush()
+  graphics.debug_buffer(buffer_1)
+  graphics.debug_buffer(buffer_2)
+end
+
+--#region Graphics object metatable
+
+--- Graphics object metatable.
+local turmitor_graphics_object_mt = {}
+
+local function delete(self)
+  for i, object in ipairs(pre_buffer) do
+    if object == self then
+      table.remove(pre_buffer, i)
+      break
+    end
+  end
+end
+
+--- Index metamethod for the graphics object metatable.
+--- Allows for getting `x1` and `y1` as aliases for `x` and `y`.
+function turmitor_graphics_object_mt.__index(self, key)
+  if key == "delete" then
+    return delete
+  end
+
+  if key == "x1" then
+    return self.x
+  end
+
+  if key == "y1" then
+    return self.y
+  end
+end
+
+--- Newindex metamethod for the graphics object metatable.
+--- Allows for setting `x1` and `y1` as aliases for `x` and `y`.
+function turmitor_graphics_object_mt.__newindex(self, key, value)
+  if key == "x1" then
+    self.x = value
+  end
+
+  if key == "y1" then
+    self.y = value
+  end
+end
+
+--#endregion
+
+--- Create a new graphics object (inject the metatable and add it to the
+--- pre-buffer).
+---@param object graphics_object The object to create.
+---@return turmitor_graphics_object object The created object.
+local function create(object)
+  setmetatable(object, turmitor_graphics_object_mt)
+
+  ---@cast object turmitor_graphics_object
+
+  pre_buffer[#pre_buffer + 1] = object
+  return object
+end
+
+--- Set the size of the screen. Warning: this method instantly clears the
+--- screen, no matter what you have set `auto_update` to.
+---@param x integer The width of the screen.
+---@param y integer The height of the screen.
+function turmitor_graphics.set_size(x, y)
+  size.x = x
+  size.y = y
+
+  buffer_1 = graphics.init_buffer(x, y)
+  buffer_2 = graphics.init_buffer(x, y)
+  TurmitorServer.clear(colors.black)
+end
+
+--- Get the size of the screen.
+---@return integer x The width of the screen.
+---@return integer y The height of the screen.
+function turmitor_graphics.get_size()
+  return size.x, size.y
+end
+
+--- Set the auto-update setting. If this is true, the screen will be updated
+--- after every change.
+---@param enabled boolean Whether to enable auto-update.
+function turmitor_graphics.set_auto_update(enabled)
+  auto_update = enabled
+end
+
+--- Get the auto-update setting.
+---@return boolean enabled Whether auto-update is enabled.
+function turmitor_graphics.get_auto_update()
+  return auto_update
+end
+
+--- Draw a pixel to the screen.
+---@param x integer The x-coordinate of the pixel.
+---@param y integer The y-coordinate of the pixel.
+---@param color color The color of the pixel.
+---@return turmitor_graphics_object|graphics_object-pixel object The created pixel object.
+function turmitor_graphics.pixel(x, y, color)
+  local object = create {
+    type = "pixel",
+    x = x,
+    y = y,
+    color = color,
+    draw_order = 0,
+    enabled = true
+  }
+  ---@cast object +graphics_object-pixel
+
+  if auto_update then
+    update_screen()
+  end
+
+  return object
+end
+
+--- Draw a line to the screen.
+---@param x1 integer The x-coordinate of the start of the line.
+---@param y1 integer The y-coordinate of the start of the line.
+---@param x2 integer The x-coordinate of the end of the line.
+---@param y2 integer The y-coordinate of the end of the line.
+---@param color color The color of the line.
+---@param thickness integer? The thickness of the line. Defaults to 1.
+---@return turmitor_graphics_object|graphics_object-line object The created line object.
+function turmitor_graphics.line(x1, y1, x2, y2, color, thickness)
+  local object = create {
+    type = "line",
+    x = x1,
+    y = y1,
+    x2 = x2,
+    y2 = y2,
+    color = color,
+    thickness = thickness or 1,
+    draw_order = 0,
+    enabled = true
+  }
+  ---@cast object +graphics_object-line
+
+  if auto_update then
+    update_screen()
+  end
+
+  return object
+end
+
+--- Draw a rectangle to the screen.
+---@param x integer The x-coordinate of the top-left corner of the rectangle.
+---@param y integer The y-coordinate of the top-left corner of the rectangle.
+---@param width integer The width of the rectangle.
+---@param height integer The height of the rectangle.
+---@param color color The color of the rectangle.
+---@param thickness integer? The thickness of the rectangle. Defaults to 1.
+---@param filled boolean? Whether the rectangle should be filled. Defaults to false.
+---@return turmitor_graphics_object|graphics_object-rectangle object The created rectangle object.
+function turmitor_graphics.rectangle(x, y, width, height, color, thickness, filled)
+  local object = create {
+    type = "rectangle",
+    x = x,
+    y = y,
+    width = width,
+    height = height,
+    color = color,
+    thickness = thickness or 1,
+    filled = filled or false,
+    draw_order = 0,
+    enabled = true
+  }
+  ---@cast object +graphics_object-rectangle
+
+  if auto_update then
+    update_screen()
+  end
+
+  return object
+end
+
+--- Draw an ellipse to the screen.
+---@param x integer The x-coordinate of the center of the ellipse.
+---@param y integer The y-coordinate of the center of the ellipse.
+---@param a integer The horizontal radius of the ellipse.
+---@param b integer The vertical radius of the ellipse. Set to 0 or a to make a circle.
+---@param color color The color of the ellipse.
+---@param thickness integer? The line thickness of the ellipse. Defaults to 1.
+---@param filled boolean? Whether the ellipse should be filled. Defaults to false.
+---@return turmitor_graphics_object|graphics_object-ellipse object The created ellipse object.
+function turmitor_graphics.ellipse(x, y, a, b, color, thickness, filled)
+  local object = create {
+    type = "ellipse",
+    x = x,
+    y = y,
+    a = a,
+    b = b,
+    color = color,
+    thickness = thickness or 1,
+    filled = filled or false,
+    draw_order = 0,
+    enabled = true
+  }
+  ---@cast object +graphics_object-ellipse
+
+  if auto_update then
+    update_screen()
+  end
+
+  return object
+end
+
+--- Draw an image to the screen.
+---@param image image The image to draw.
+---@param x integer The x-coordinate of the top-left corner of the image.
+---@param y integer The y-coordinate of the top-left corner of the image.
+function turmitor_graphics.image(image, x, y)
+  error("Not yet implemented.")
+end
+
+--- Draw text to the screen.
+---@param text string The text to draw.
+---@param x integer The x-coordinate of the text.
+---@param y integer The y-coordinate of the text.
+---@param fg color? The foreground color of the text. Leave nil for white.
+---@param bg color? The background color of the text. Leave nil for transparent.
+---@param font unknown? The font to use for the text.
+function turmitor_graphics.text(text, x, y, fg, bg, font)
+  error("Not yet implemented.")
+end
+
+--- Manually update the screen.
+function turmitor_graphics.flush()
+  update_screen()
+end
+
+turmitor_graphics.set_size(size.x, size.y)
+return turmitor_graphics
